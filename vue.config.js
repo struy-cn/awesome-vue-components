4d8bec5c0885b8b712a87353457fe469dbddffc6
struy <1724555319@qq.com>
2022-10-20 11:45:27 +0800

    fix it

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/awesome-vue-components/',
+  publicPath: '/awesome-vue-components/',
   assetsDir: 'static',
   transpileDependencies: true,
   devServer: {
