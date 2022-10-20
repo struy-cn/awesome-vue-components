e76b5647b529b00b0235b2ca066f86ae80bbbedc
struy <1724555319@qq.com>
2022-10-20 11:39:21 +0800

    gpage

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  base: '/awesome-vue-components/',
   assetsDir: 'static',
   transpileDependencies: true,
   devServer: {
